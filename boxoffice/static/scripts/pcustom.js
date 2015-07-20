e6c7ff6db16c1487a6d9012962aee722b66ad176
Pardis <ppashakhanloo@gmail.com>
2015-07-20 21:21:18 +0430

    bug fixes

--- boxoffice/static/scripts/pcustom.js
+++ boxoffice/static/scripts/pcustom.js
@@ -8,36 +8,16 @@ $('#inputRate').on('rating.change', function(event, value, caption) {
     // we'll use this for saving rate and ...
 });
 
-
-// use this to convert all numbers to persian
-// $(document).ready(function() {
-//     persian={0:'۰',1:'۱',2:'۲',3:'۳',4:'۴',5:'۵',6:'۶',7:'۷',8:'۸',9:'۹'};
-//     function traverse(el){
-//         if(el.nodeType==3){
-//             var list=el.data.match(/[0-9]/g);
-//             if(list!=null && list.length!=0){
-//                 for(var i=0;i<list.length;i++)
-//                     el.data=el.data.replace(list[i],persian[list[i]]);
-//             }
-//         }
-//         for(var i=0;i<el.childNodes.length;i++){
-//             traverse(el.childNodes[i]);
-//         }
-//     }
-//     traverse(document.body);
-// });
-
-    
-    // start proto - global vars
-    var ticketType =0;
-    var ticketPrice=0;
-    var ticketNum=0;
-    // start proto
+    // // start proto - global vars
+    // var ticketType =0;
+    // var ticketPrice=0;
+    // var ticketNum=0;
+    // // start proto
 
 // functionality is only for prototype, we'll erase it later.
 $(document).ready(function() {
     var navListItems = $('ul.setup-panel li a'),
-        allWells = $('.setup-content');
+    allWells = $('.setup-content');
 
     allWells.hide();
 
@@ -45,7 +25,7 @@ $(document).ready(function() {
     {
         e.preventDefault();
         var $target = $($(this).attr('href')),
-            $item = $(this).closest('li');
+        $item = $(this).closest('li');
         
         if (!$item.hasClass('disabled')) {
             navListItems.closest('li').removeClass('active');
@@ -76,28 +56,28 @@ $(document).ready(function() {
             $('ul.setup-panel li:eq(0)').addClass('disabled');
             $('ul.setup-panel li:eq(1)').removeClass('disabled');
             $('ul.setup-panel li a[href="#step-2"]').trigger('click');
-            ticketType = document.getElementById('type1').innerHTML;
-            ticketPrice = document.getElementById('price1').innerHTML;
-            ticketNum = document.getElementById('num1').value;
-            flag = true;
+            // ticketType = document.getElementById('type1').innerHTML;
+            // ticketPrice = document.getElementById('price1').innerHTML;
+            // ticketNum = document.getElementById('num1').value;
+            // flag = true;
         }
         else if (document.getElementById('radio2').checked) {
             $('ul.setup-panel li:eq(0)').addClass('disabled');
             $('ul.setup-panel li:eq(1)').removeClass('disabled');
             $('ul.setup-panel li a[href="#step-2"]').trigger('click');
-            ticketType = document.getElementById('type2').innerHTML;
-            ticketPrice = document.getElementById('price2').innerHTML;
-            ticketNum = document.getElementById('num2').value;
-            flag = true;
+            // ticketType = document.getElementById('type2').innerHTML;
+            // ticketPrice = document.getElementById('price2').innerHTML;
+            // ticketNum = document.getElementById('num2').value;
+            // flag = true;
         }
         else if (document.getElementById('radio3').checked) {
             $('ul.setup-panel li:eq(0)').addClass('disabled');
             $('ul.setup-panel li:eq(1)').removeClass('disabled');
-            $('ul.setup-panel li a[href="#step-2"]').trigger('click');
-            ticketType = document.getElementById('type3').innerHTML;
-            ticketPrice = document.getElementById('price3').innerHTML;
-            ticketNum = document.getElementById('num3').value;
-            flag = true;
+            $('ul.setup-panel li a[href="#step-2"]').trigger('click');               
+            // ticketType = document.getElementById('type3').innerHTML;
+            // ticketPrice = document.getElementById('price3').innerHTML;
+            // ticketNum = document.getElementById('num3').value;
+            // flag = true;
         }
         else { // none is chosen
             var error = document.getElementById('error-msg1');
@@ -106,49 +86,67 @@ $(document).ready(function() {
                 error.style.visibility = 'hidden';
             }, 3000);
         }
-        if(flag) {
-            document.getElementById('factorBody').innerHTML = "";
-            var newTr;
-            for(var i = 0; i < parseInt(ticketNum); i++) {
-                newTr = document.createElement('tr');
-                newTr.innerHTML += '<td>'+ticketType+'</td>'
-                    +'<td>برج میلاد</td>'
-                    +'<td>۲۱ آبان ۹۴</td>'
-                    +'<td>۱۸:۳۰</td>'
-                    +'<td>'+ (i+12) +'</td>'
-                    +'<td>'+ticketPrice+'</td>';
-                document.getElementById('factorBody').appendChild(newTr);
-            }
+        // if(flag) {
+        //     document.getElementById('factorBody').innerHTML = "";
+        //     var newTr;
+        //     for(var i = 0; i < parseInt(ticketNum); i++) {
+        //         newTr = document.createElement('tr');
+        //         newTr.innerHTML += '<td>'+ticketType+'</td>'
+        //             +'<td>برج میلاد</td>'
+        //             +'<td>۲۱ آبان ۹۴</td>'
+        //             +'<td>۱۸:۳۰</td>'
+        //             +'<td>'+ (i+12) +'</td>'
+        //             +'<td>'+ticketPrice+'</td>';
+        //         document.getElementById('factorBody').appendChild(newTr);
+        //     }
 
-            newTr = document.createElement('tr');
-            var tp = (parseInt(ticketNum)*parseInt(ticketPrice));
-            newTr.innerHTML = '<tr><td colspan="20" style="text-align: left">مبلغ کل : <span id="totalPrice">'+ tp +'</span> تومان</td></tr>'
-            document.getElementById('factorBody').appendChild(newTr);
-        }
+        //     newTr = document.createElement('tr');
+        //     var tp = (parseInt(ticketNum)*parseInt(ticketPrice));
+        //     newTr.innerHTML = '<tr><td colspan="20" style="text-align: left">مبلغ کل : <span id="totalPrice">'+ tp +'</span> تومان</td></tr>'
+        //     document.getElementById('factorBody').appendChild(newTr);
+        // }
         // end proto
 
         // this part must be commented for prototype, but it is actually main part!!!
-        // $('ul.setup-panel li:eq(0)').addClass('disabled');
-        // $('ul.setup-panel li:eq(1)').removeClass('disabled');
-        // $('ul.setup-panel li a[href="#step-2"]').trigger('click');
+        $('ul.setup-panel li:eq(0)').addClass('disabled');
+        $('ul.setup-panel li:eq(1)').removeClass('disabled');
+        $('ul.setup-panel li a[href="#step-2"]').trigger('click');
     });
 
-    $('#activate-step-3').on('click', function(e) {
-        e.preventDefault();
-        $('ul.setup-panel li:eq(0)').addClass('disabled');
-        $('ul.setup-panel li:eq(1)').addClass('disabled');
-        $('ul.setup-panel li:eq(2)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#step-3"]').trigger('click');
-        
+$('#activate-step-3').on('click', function(e) {
+    e.preventDefault();
+    $('ul.setup-panel li:eq(0)').addClass('disabled');
+    $('ul.setup-panel li:eq(1)').addClass('disabled');
+    $('ul.setup-panel li:eq(2)').removeClass('disabled');
+    $('ul.setup-panel li a[href="#step-3"]').trigger('click');
+    
         // start proto
-        var tp = (parseInt(ticketNum)*parseInt(ticketPrice));
-        document.getElementById('acceptedTotalPrice').innerHTML = tp;
+        // var tp = (parseInt(ticketNum)*parseInt(ticketPrice));
+        // document.getElementById('acceptedTotalPrice').innerHTML = tp;
         // end proto
     });
 });
 
+// start proto
+document.getElementById('radio1').addEventListener('click', function() {
+    document.getElementById('num1').disabled = false;
+    document.getElementById('num2').disabled = true;
+    document.getElementById('num3').disabled = true;
+});
+document.getElementById('radio2').addEventListener('click', function() {
+    document.getElementById('num1').disabled = true;
+    document.getElementById('num2').disabled = false;
+    document.getElementById('num3').disabled = true;
+});
+document.getElementById('radio3').addEventListener('click', function() {
+    document.getElementById('num1').disabled = true;
+    document.getElementById('num2').disabled = true;
+    document.getElementById('num3').disabled = false;
+});
+// end proto
+
 $("td").click(function () {
-   $(this).find('input:radio').attr('checked', true);
+ $(this).find('input:radio').attr('checked', true);
 });
 
 // start proto
@@ -156,15 +154,14 @@ document.getElementById('approvePay').addEventListener('click', function(e) {
     e.preventDefault();
     var inputCardNum = document.getElementById('inputCardNum').value;
     if(inputCardNum === "1111-1111-1111-1111") {
-        window.location.href = "view-receipt.html";
+        window.location.href = "/orders/1/receipt/";
     }
-
     else {
-                    var error = document.getElementById('error-msg2');
-            error.style.visibility = 'visible';
-            setTimeout(function() {
-                error.style.visibility = 'hidden';
-            }, 3000);
+        var error = document.getElementById('error-msg2');
+        error.style.visibility = 'visible';
+        setTimeout(function() {
+            error.style.visibility = 'hidden';
+        }, 3000);
     }
 });
 // end proto
