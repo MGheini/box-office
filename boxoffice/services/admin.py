97f0f24a3b3fc1d0fbbc9b5dcb2b239361e8c2fa
Pardis <ppashakhanloo@gmail.com>
2015-07-21 16:50:12 +0430

    جستجوی سفارشها در بازه زمانی مشخص افزوده شد

--- boxoffice/services/admin.py
+++ boxoffice/services/admin.py
@@ -1,11 +1,18 @@
  
 from django.contrib import admin
-
+from daterange_filter.filter import DateRangeFilter
 from .models import Event, Feedback, Ticket, Order, Category, SubCategory
 
-admin.site.register(Event)
+class EventAdmin(admin.ModelAdmin):
+	list_filter = ('category', 'subcategory', 'organizer')
+
+class OrderAdmin(admin.ModelAdmin):
+	list_filter = ('order_date', ('order_date', DateRangeFilter))
+	
+
+admin.site.register(Event, EventAdmin)
 admin.site.register(Feedback)
 admin.site.register(Ticket)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Category)
 admin.site.register(SubCategory)
