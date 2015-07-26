62705a611e612fc309fad18b6400ebd43c989abf
Pardis <ppashakhanloo@gmail.com>
2015-07-26 16:36:10 +0430

    admin urls added.

--- boxoffice/boxoffice_admin/views.py
+++ boxoffice/boxoffice_admin/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .boxoffice_admin import models
 
 # Create your views here.
+def delete_multiple_events(request):
+	events = models.Event.objects.all()
+
+	if request.method == 'POST':
+		todel = request.POST.getlist('todelete')
+		models.Event.objects.filter(id__in=todel).delete()
+		return HttpResponseRedirect('')
+
+	return render(request, 'delete-mutiple-events.html', {'events': events})
