64f1cdd64b157e1bfe9ad9a7eb184757f04fd4fb
Pardis <ppashakhanloo@gmail.com>
2015-08-02 19:22:24 +0430

    datetime field added to add event.

--- boxoffice/services/forms.py
+++ boxoffice/services/forms.py
@@ -31,8 +31,8 @@ class EventModelForm(forms.ModelForm):
 		}
 		widgets = {
           		'event_description': forms.Textarea(attrs={'rows':4, 'maxlength': 255, 'placeholder': ' مثلا: نیم ساعت قبل از شروع برنامه در سالن حضور داشته باشید.'}),
-			'event_deadline': forms.TextInput(attrs={'id':'date_input', 'placeholder': '1394-04-05 20:14'}),
-			'event_date': forms.TextInput(attrs={'id':'date_input_2', 'placeholder': '1394-04-05 20:14'}),
+			'event_deadline': forms.TextInput(attrs={'id':'date_input', 'placeholder': '1394-04-05 20:14', 'type': 'datetime-local'}),
+			'event_date': forms.TextInput(attrs={'id':'date_input_2', 'placeholder': '1394-04-05 20:14', 'type': 'datetime-local'}),
 			'event_place': forms.TextInput(attrs={'placeholder': ' استادیوم آزادی'}),
 			'event_title': forms.TextInput(attrs={'placeholder': ' مسابقه والیبال ایران-آمریکا'}),
         	}
