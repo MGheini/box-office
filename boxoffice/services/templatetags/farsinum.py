7fba99a5ac55a6f58d7e85b970eb89b639325b61
unknown <ppashakhanloo@gmail.com>
2015-08-14 20:00:21 +0430

    bug fix in farsinum

--- boxoffice/services/templatetags/farsinum.py
+++ boxoffice/services/templatetags/farsinum.py
@@ -9,6 +9,9 @@ def farsi_num( value ):
 
   farsi_num = ''
   for i in str(value):
+    if i == '0' or i == '1' or i == '2' or i == '3' or i == '4' or i == '5' or i == '6' or i == '7' or i == '8' or i == '9':
       farsi_num += en_2_fa[i]
+    else:
+      farsi_num += i
 
   return farsi_num
